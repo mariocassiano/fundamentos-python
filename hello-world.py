dcb946035b346546da168d8501a298f233ebf919
Nomade56 <joaovictor.amancio56@hotmail.com>
2024-09-11 20:09:14 -0300

    initial commit

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+class HelloWorld:
+    def __init__(self):
+        self.message = "Hello, World!"
+    
+    def display_message(self):
+        print(self.message)
+
+# Criando uma instância da classe e chamando o método para exibir a mensagem
+hello = HelloWorld()
+hello.display_message()
