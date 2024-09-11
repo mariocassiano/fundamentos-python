7272cba794e8cc91ea841ddc5a21ffa66e63c22a
Nomade56 <joaovictor.amancio56@hotmail.com>
2024-09-11 20:10:01 -0300

    feat: entrada de dados

---
+++ entrada_dados.py
@@ -0,0 +1,18 @@
+# Entrada de dados
+nome = input("Digite seu nome: ")
+
+#Saída de dados
+print(nome)
+
+# Tipos de dados primitivos
+inteiro = 10
+flutuantes = 20.5
+sequencia_caracteres = "uma string"
+booleano = True
+
+# Conversão entre tipos
+ano_nascimento = int(input("Digite o ano em que você nasceu: "))
+idade = 2024 - ano_nascimento
+
+# String literal
+print(f"A sua idade é: {idade}")
